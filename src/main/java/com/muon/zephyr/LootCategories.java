de9ade8a1f554c9bf2d68fd7c957e57cda3bc3eb
muon-rw <pashah012@gmail.com>
2023-10-24 00:45:37 -0500

    Loot Category changes

--- src/main/java/com/muon/zephyr/LootCategories.java
+++ src/main/java/com/muon/zephyr/LootCategories.java
@@ -2,20 +2,21 @@ package com.muon.zephyr;
 
 import dev.shadowsoffire.apotheosis.adventure.loot.LootCategory;
 import net.minecraft.world.entity.EquipmentSlot;
-//import net.minecraft.entity.EquipmentSlot;
-//import net.minecraft.entity.attribute.EntityAttributes;
-//import net.minecraft.item.ItemStack;
-import net.minecraft.world.entity.ai.attributes.Attributes;
+// import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
-import net.spell_engine.api.item.weapon.SpellWeaponItem;
+// import net.spell_engine.api.item.weapon.SpellWeaponItem;
 // import net.spell_engine.api.item.weapon.StaffItem;
 import net.spell_power.api.MagicSchool;
 import net.spell_power.api.attributes.EntityAttributes_SpellPower;
 
 public class LootCategories {
     // public static final LootCategory STAFF = LootCategory.register(LootCategory.SWORD, "STAFF", s-> s.getItem() instanceof StaffItem, arr(EquipmentSlot.MAINHAND);
-    public static final LootCategory SPELL_WEAPON = LootCategory.register(LootCategory.SWORD, "SPELL_WEAPON", s-> s.getItem() instanceof SpellWeaponItem && s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(Attributes.ATTACK_DAMAGE).stream().anyMatch((m) -> m.getAmount() > 4.0), arr(EquipmentSlot.MAINHAND));
-
+    // public static final LootCategory SPELL_WEAPON = LootCategory.register(LootCategory.SWORD, "SPELL_WEAPON", s-> s.getItem() instanceof SpellWeaponItem && s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(Attributes.ATTACK_DAMAGE).stream().anyMatch((m) -> m.getAmount() > 4.0), arr(EquipmentSlot.MAINHAND));
+    public static final LootCategory ELEMENTAL_STAFF = LootCategory.register(LootCategory.SWORD, "ELEMENTAL_STAFF", s->
+            s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.ARCANE)).stream().anyMatch((m) -> m.getAmount() > 0.0) &&
+            s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.FIRE)).stream().anyMatch((m) -> m.getAmount() > 0.0) &&
+            s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.FROST)).stream().anyMatch((m) -> m.getAmount() > 0.0),
+            arr(EquipmentSlot.MAINHAND));
     public static final LootCategory FIRE_STAFF = LootCategory.register(LootCategory.SWORD, "FIRE_STAFF", s-> s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.FIRE)).stream().anyMatch((m) -> m.getAmount() > 0.0), arr(EquipmentSlot.MAINHAND));
     public static final LootCategory FROST_STAFF = LootCategory.register(LootCategory.SWORD, "FROST_STAFF", s-> s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.FROST)).stream().anyMatch((m) -> m.getAmount() > 0.0), arr(EquipmentSlot.MAINHAND));
     public static final LootCategory ARCANE_STAFF = LootCategory.register(LootCategory.SWORD, "ARCANE_STAFF", s-> s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.ARCANE)).stream().anyMatch((m) -> m.getAmount() > 0.0), arr(EquipmentSlot.MAINHAND));
@@ -23,11 +24,11 @@ public class LootCategories {
     public static final LootCategory HEALING_STAFF = LootCategory.register(LootCategory.SWORD, "HEALING_STAFF", s-> s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.HEALING)).stream().anyMatch((m) -> m.getAmount() > 0.0), arr(EquipmentSlot.MAINHAND));
     public static final LootCategory LIGHTNING_STAFF = LootCategory.register(LootCategory.SWORD, "LIGHTNING_STAFF", s-> s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.LIGHTNING)).stream().anyMatch((m) -> m.getAmount() > 0.0), arr(EquipmentSlot.MAINHAND));
 
+
     private static EquipmentSlot[] arr(EquipmentSlot... s) {
         return s;
     }
-    // public static boolean isStaffItem(ItemStack stack) { return LootCategory.forItem(stack).equals(STAFF);}
-    public static boolean isSpellWeapon(ItemStack stack) { return LootCategory.forItem(stack).equals(SPELL_WEAPON);}
+    public static boolean isElementalStaff(ItemStack stack) { return LootCategory.forItem(stack).equals(ELEMENTAL_STAFF);}
     public static boolean isFireStaff(ItemStack stack) { return LootCategory.forItem(stack).equals(FIRE_STAFF);}
     public static boolean isFrostStaff(ItemStack stack) { return LootCategory.forItem(stack).equals(FROST_STAFF);}
     public static boolean isArcaneStaff(ItemStack stack) { return LootCategory.forItem(stack).equals(ARCANE_STAFF);}
