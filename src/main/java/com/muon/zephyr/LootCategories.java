8fc39656adf5d881a530da57a77f3abd2578124f
muon-rw <pashah012@gmail.com>
2023-11-29 20:36:17 -0600

    Clean up

--- src/main/java/com/muon/zephyr/LootCategories.java
+++ src/main/java/com/muon/zephyr/LootCategories.java
@@ -30,19 +30,8 @@ public class LootCategories {
     public static final LootCategory SOUL_STAFF = LootCategory.register(LootCategory.SWORD, "SOUL_STAFF", s-> s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.SOUL)).stream().anyMatch((m) -> m.getAmount() > 0.0), arr(EquipmentSlot.MAINHAND));
     public static final LootCategory HEALING_STAFF = LootCategory.register(LootCategory.SWORD, "HEALING_STAFF", s-> s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.HEALING)).stream().anyMatch((m) -> m.getAmount() > 0.0), arr(EquipmentSlot.MAINHAND));
     public static final LootCategory LIGHTNING_STAFF = LootCategory.register(LootCategory.SWORD, "LIGHTNING_STAFF", s-> s.getItem().getAttributeModifiers(s, EquipmentSlot.MAINHAND).get(EntityAttributes_SpellPower.POWER.get(MagicSchool.LIGHTNING)).stream().anyMatch((m) -> m.getAmount() > 0.0), arr(EquipmentSlot.MAINHAND));
-    //public static final LootCategory TWO_HANDED = LootCategory.register(LootCategory.SWORD, "TWO_HANDED", itemStack -> { WeaponAttributes weaponAttributes = WeaponRegistry.getAttributes(itemStack); return weaponAttributes != null && weaponAttributes.isTwoHanded();}, arr(EquipmentSlot.MAINHAND));
-    public static LootCategory forItem(ItemStack item) {
-        LootCategory override = AdventureConfig.TYPE_OVERRIDES.get(BuiltInRegistries.ITEM.getKey(item.getItem()));
-        if (override != null) return override;
-        for (LootCategory c : VALUES) {
-            if (c.isValid(item)) return c;
-        }
-        WeaponAttributes weaponAttributes = WeaponRegistry.getAttributes(item);
-        if (weaponAttributes != null && weaponAttributes.isTwoHanded()) {
-            return HEAVY_WEAPON;
-        }
-        return LootCategory.NONE;
-    }
+    // We add to the HEAVY_WEAPON criteria with a Mixin instead.
+    // public static final LootCategory TWO_HANDED = LootCategory.register(LootCategory.SWORD, "TWO_HANDED", itemStack -> { WeaponAttributes weaponAttributes = WeaponRegistry.getAttributes(itemStack); return weaponAttributes != null && weaponAttributes.isTwoHanded();}, arr(EquipmentSlot.MAINHAND))
     private static EquipmentSlot[] arr(EquipmentSlot... s) {
         return s;
     }
@@ -53,7 +42,7 @@ public class LootCategories {
     public static boolean isSoulStaff(ItemStack stack) { return LootCategory.forItem(stack).equals(SOUL_STAFF);}
     public static boolean isHealingStaff(ItemStack stack) { return LootCategory.forItem(stack).equals(HEALING_STAFF);}
     public static boolean isLightningStaff(ItemStack stack) { return LootCategory.forItem(stack).equals(LIGHTNING_STAFF);}
-    //public static boolean isTwoHanded(ItemStack stack) { return LootCategory.forItem(stack).equals(TWO_HANDED);}
+    // public static boolean isTwoHanded(ItemStack stack) { return LootCategory.forItem(stack).equals(TWO_HANDED);}
 
     public static void init() {}
 
