ff433acaeaf37eb2ca4ee829138c0f45d2c46a87
muon-rw <pashah012@gmail.com>
2024-07-08 09:59:34 -0500

    Update for Zenith 1.2.1

--- src/main/java/dev/muon/zephyr/mixin/LootRarityMixin.java
+++ src/main/java/dev/muon/zephyr/mixin/LootRarityMixin.java
@@ -3,6 +3,8 @@ package dev.muon.zephyr.mixin;
 import dev.muon.zephyr.AffixSchoolMapper;
 import dev.muon.zephyr.LootCategories;
 import dev.shadowsoffire.apotheosis.adventure.affix.Affix;
+import dev.shadowsoffire.apotheosis.adventure.affix.AffixType;
+import dev.shadowsoffire.apotheosis.adventure.loot.LootController;
 import dev.shadowsoffire.apotheosis.adventure.loot.LootRarity;
 import dev.shadowsoffire.placebo.reload.DynamicHolder;
 import net.minecraft.world.item.ItemStack;
@@ -11,26 +13,27 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 @Mixin(value = LootRarity.LootRule.class, remap = false)
 public abstract class LootRarityMixin {
-    @Redirect(method = "execute", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;", remap = false))
-    private Stream<DynamicHolder<Affix>> filterAffixes(Stream<DynamicHolder<Affix>> stream, Predicate<DynamicHolder<Affix>> predicate, ItemStack stack, LootRarity rarity, Set<DynamicHolder<Affix>> currentAffixes) {
+    @Redirect(method = "execute", at = @At(value = "INVOKE", target = "Ldev/shadowsoffire/apotheosis/adventure/loot/LootController;getAvailableAffixes(Lnet/minecraft/world/item/ItemStack;Ldev/shadowsoffire/apotheosis/adventure/loot/LootRarity;Ljava/util/Set;Ldev/shadowsoffire/apotheosis/adventure/affix/AffixType;)Ljava/util/List;"))
+    private List<DynamicHolder<? extends Affix>> filterAffixes(ItemStack stack, LootRarity rarity, Set<DynamicHolder<? extends Affix>> currentAffixes, AffixType type) {
+        List<DynamicHolder<? extends Affix>> availableAffixes = LootController.getAvailableAffixes(stack, rarity, currentAffixes, type);
         Set<SpellSchool> gearSpellSchools = AffixSchoolMapper.getSpellSchoolsFromWeapon(stack);
-        return stream.filter(a -> {
-            if (!predicate.test(a)) {
-                return false;
-            }
-            Affix affix = a.get();
-            String affixId = affix.getId().toString();
-            if (AffixSchoolMapper.isElementalAffix(affixId)) {
-                SpellSchool affixSpellSchool = AffixSchoolMapper.getSpellSchoolForAffix(affixId);
-                return affixSpellSchool != null && gearSpellSchools.contains(affixSpellSchool);
-            }
-            return true;
-        });
+
+        return availableAffixes.stream()
+                .filter(a -> {
+                    Affix affix = a.get();
+                    String affixId = affix.getId().toString();
+                    if (AffixSchoolMapper.isElementalAffix(affixId)) {
+                        SpellSchool affixSpellSchool = AffixSchoolMapper.getSpellSchoolForAffix(affixId);
+                        return affixSpellSchool != null && gearSpellSchools.contains(affixSpellSchool);
+                    }
+                    return true;
+                })
+                .collect(Collectors.toList());
     }
 }
